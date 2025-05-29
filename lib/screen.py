f4c3bf4649937fe2ca63cd860d60bc99221adb75
Joshua Thompson <jt244345@falmouth.ac.uk>
2025-05-29 18:08:58 +0100

    fix: test

--- lib/screen.py
+++ lib/screen.py
@@ -48,7 +48,7 @@ def display_text(textData: List[Tuple[str, float]], fontType):
         h: int = (s / txtScale) * HEIGHT #
         w: int = (WIDTH - tWidth) // 2 # Centers on X-axis
 
-        draw.text((0, HEIGHT//2), t, font=font, fill=0, anchor="mm", align="center")
+        draw.text((WIDTH//2, HEIGHT//2), t, font=font, fill=0, anchor="mm", align="center")
 
 
 

0e4788110f1e6fee600555bcaa24e8249f23d923
Joshua Thompson <jt244345@falmouth.ac.uk>
2025-06-03 00:02:42 +0100

    feat: testing scaling

--- commands/ping.py
+++ commands/ping.py
@@ -5,6 +5,8 @@ libdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib')
 if os.path.exists(libdir):
     sys.path.append(libdir)
 
+import screen as s
+
 class Ping(command_base.Command):
     name: str = "ping"
     description: str = "responds if working"
@@ -15,4 +17,7 @@ class Ping(command_base.Command):
     def run(self, *args, **kwargs):
         rslt = super().run(*args, **kwargs)
         print("pong!")
+
+        screen = s.Screen.instance
+        screen.set(screen.TEXT.create_wrapper([screen.WIDTH, screen.HEIGHT], "scale", 0, ["This is a test of auto scaling font size", "h1", "center"]))
         return rslt

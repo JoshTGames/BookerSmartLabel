7f73a6456a18e10d44a0656acc4432e57ebcdc47
Joshua Thompson <jt244345@falmouth.ac.uk>
2025-06-04 11:46:16 +0100

    feat: trying to keep the main thread alive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import command_handler, screen as s, speech_recog as sr
+import time
 MANAGER : command_handler.CommandHandler = command_handler.CommandHandler()
 SPEECH : sr.SpeechRecognition = sr.SpeechRecognition(MANAGER)
 SCREEN : s.Screen = s.Screen()
@@ -6,7 +7,8 @@ SCREEN : s.Screen = s.Screen()
 # CMDLINE
 
 # DOESNT WORK IN SYSTEMCTL
-# while True:
+while True:
+    time.sleep(1)
 #     cmd = str(input("Command:\t")).split()
 #     if(len(cmd) <= 0): continue
 #     MANAGER.execute(cmd[0], *cmd[1:])
